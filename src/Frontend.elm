77e5a91dbc0e243fa3c45564e0c4f5e708dc92a0
Dusan Stevanovic <dusan.stevanovic@kindredgroup.com>
2023-05-31 18:01:41 +0200

    Add editable UI

--- src/Frontend.elm
+++ src/Frontend.elm
@@ -11,8 +11,7 @@ import Html.Styled.Events exposing (onClick, onInput)
 import Lamdera exposing (sendToBackend)
 import Ports
 import Process
-import Svg.Styled exposing (g, path, svg)
-import Svg.Styled.Attributes as SvgAttr
+import Svgs
 import Tailwind.Breakpoints as Bp
 import Tailwind.Theme as Tw
 import Tailwind.Utilities as Tw
@@ -722,7 +721,10 @@ view model =
                                                 [ Tw.gap_10, Tw.items_center, Tw.flex_row ]
                                             ]
                                         ]
-                                        [ Html.h2 [ Attr.css [ Tw.m_0, Tw.break_all ] ] [ model.roomName |> Maybe.withDefault "Room name is not available" |> text ]
+                                        [ Html.h2 [ Attr.class "edit", Attr.css [ Tw.m_0, Tw.break_all, Tw.flex, Tw.items_center, Tw.cursor_pointer, Tw.cursor_pointer, Tw.self_start, Tw.pl_2 ] ]
+                                            [ model.roomName |> Maybe.withDefault "Room name is not available" |> text
+                                            , Html.span [ Attr.css [ Tw.ml_2 ] ] [ Svgs.withOverrideStyle |> Svgs.iconPencil ]
+                                            ]
                                         , case model.card of
                                             Just _ ->
                                                 if model.shouldShowCharts then
@@ -827,7 +829,10 @@ view model =
                                         (model.stories
                                             |> List.map
                                                 (\story ->
-                                                    Html.li [ Attr.css [ Tw.break_all ] ] [ text story ]
+                                                    Html.li [ Attr.class "edit", Attr.css [ Tw.break_all, Tw.flex, Tw.items_center, Tw.cursor_pointer, Tw.self_start, Tw.pl_2 ] ]
+                                                        [ text story
+                                                        , Html.span [ Attr.css [ Tw.ml_2 ] ] [ Svgs.withOverrideStyle |> Svgs.iconPencil ]
+                                                        ]
                                                 )
                                         )
                                     ]
@@ -1300,24 +1305,7 @@ viewNotifications { error, announcement } =
                 , Tw.bg_color Tw.red_200
                 ]
             ]
-            [ svg
-                [ Attr.css
-                    [ Tw.flex_shrink_0
-                    , Tw.inline
-                    , Tw.w_7
-                    , Tw.h_7
-                    , Tw.mr_3
-                    ]
-                , SvgAttr.fill "currentColor"
-                , SvgAttr.viewBox "0 0 20 20"
-                ]
-                [ path
-                    [ SvgAttr.fillRule "evenodd"
-                    , SvgAttr.d "M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                    , SvgAttr.clipRule "evenodd"
-                    ]
-                    []
-                ]
+            [ Svgs.iconError
             , case error of
                 Just err ->
                     text err
@@ -1356,32 +1344,7 @@ viewNotifications { error, announcement } =
                                 , Tw.duration_500
                                 ]
                             ]
-                            [ svg
-                                [ SvgAttr.height "22px"
-                                , SvgAttr.width "22px"
-                                , SvgAttr.version "1.1"
-                                , SvgAttr.viewBox "0 0 512 512"
-                                , SvgAttr.xmlSpace "preserve"
-                                , Attr.css
-                                    [ Tw.flex_shrink_0
-                                    , Tw.inline
-                                    , Tw.w_7
-                                    , Tw.h_7
-                                    , Tw.mr_3
-                                    ]
-                                , SvgAttr.fill "currentColor"
-                                ]
-                                [ g []
-                                    [ path
-                                        [ SvgAttr.d "M474.045,173.813c-4.201,1.371-6.494,5.888-5.123,10.088c7.571,23.199,11.411,47.457,11.411,72.1   c0,62.014-24.149,120.315-68,164.166s-102.153,68-164.167,68s-120.316-24.149-164.167-68S16,318.014,16,256   S40.149,135.684,84,91.833s102.153-68,164.167-68c32.889,0,64.668,6.734,94.455,20.017c28.781,12.834,54.287,31.108,75.81,54.315   c3.004,3.239,8.066,3.431,11.306,0.425c3.24-3.004,3.43-8.065,0.426-11.306c-23-24.799-50.26-44.328-81.024-58.047   C317.287,15.035,283.316,7.833,248.167,7.833c-66.288,0-128.608,25.813-175.48,72.687C25.814,127.392,0,189.712,0,256   c0,66.287,25.814,128.607,72.687,175.479c46.872,46.873,109.192,72.687,175.48,72.687s128.608-25.813,175.48-72.687   c46.873-46.872,72.687-109.192,72.687-175.479c0-26.332-4.105-52.26-12.201-77.064   C482.762,174.736,478.245,172.445,474.045,173.813z"
-                                        ]
-                                        []
-                                    , path
-                                        [ SvgAttr.d "M504.969,83.262c-4.532-4.538-10.563-7.037-16.98-7.037s-12.448,2.499-16.978,7.034l-7.161,7.161   c-3.124,3.124-3.124,8.189,0,11.313c3.124,3.123,8.19,3.124,11.314-0.001l7.164-7.164c1.51-1.512,3.52-2.344,5.66-2.344   s4.15,0.832,5.664,2.348c1.514,1.514,2.348,3.524,2.348,5.663s-0.834,4.149-2.348,5.663L217.802,381.75   c-1.51,1.512-3.52,2.344-5.66,2.344s-4.15-0.832-5.664-2.348L98.747,274.015c-1.514-1.514-2.348-3.524-2.348-5.663   c0-2.138,0.834-4.149,2.351-5.667c1.51-1.512,3.52-2.344,5.66-2.344s4.15,0.832,5.664,2.348l96.411,96.411   c1.5,1.5,3.535,2.343,5.657,2.343s4.157-0.843,5.657-2.343l234.849-234.849c3.125-3.125,3.125-8.189,0-11.314   c-3.124-3.123-8.189-3.123-11.313,0L212.142,342.129l-90.75-90.751c-4.533-4.538-10.563-7.037-16.98-7.037   s-12.448,2.499-16.978,7.034c-4.536,4.536-7.034,10.565-7.034,16.977c0,6.412,2.498,12.441,7.034,16.978l107.728,107.728   c4.532,4.538,10.563,7.037,16.98,7.037c6.417,0,12.448-2.499,16.977-7.033l275.847-275.848c4.536-4.536,7.034-10.565,7.034-16.978   S509.502,87.794,504.969,83.262z"
-                                        ]
-                                        []
-                                    ]
-                                ]
+                            [ Svgs.iconCheck
                             , text info
                             ]
                     )
